2b288dc3669faffff3536da9cb0fc4011c15abf6
thomas-ode <thomas.ode.w@gmail.com>
2020-11-19 21:12:22 +0100

    before pull

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import './App.css';
 import Error from './Components/Home/Error'
+import Alcohol from './Components/Alcohol_list/Alcohol'
+import Weapons from './Components/Weapons_list/Weapons'
+import Drugs from './Components/Drugs_list/Drugs'
 
 function App() {
   return (
     <div className="App">
-      <Error />
+      <Weapons />
+      
     </div>
   );
 }
