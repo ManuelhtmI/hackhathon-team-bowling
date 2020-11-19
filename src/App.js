96243b68557ac8fb04a78a142bef8d80de9bab70
fatima-dah <dahnini.fatima@hotmail.fr>
2020-11-19 17:23:23 +0100

    container-Articles

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/navbar/Navbar'
+import Articles from './components/Articles/Articles'
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Articles />
     </div>
   );
 }
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Articles from './components/Articles/Articles.jsx'
+
+function App() {
+  return (
+    <div className="App">
+    <Articles />
+    
+    </div>
+  );
+}
+
+export default App;
